1178e77789afe2ced459d75152ebcec7959879d6
Lensatom <ayomidetommiwa@gmail.com>
2022-09-08 05:06:36 -0700

    Printing sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-	int c = sizeof(char)
-	int i = sizeof(int)
-	int li = sizeof(long int)
-	int lli = sizeof(long long int)
-	int f = sizeof(float)
+	int c = sizeof(char);
+	int i = sizeof(int);
+	int li = sizeof(long int);
+	int lli = sizeof(long long int);
+	int f = sizeof(float);
 
-	printf("Size of a char: %d byte(s)", c);
-	printf("Size of an int: %d byte(s)", i);
-	printf("Size of a long int: %d byte(s)", li);
-	printf("Size of a long long int: %d byte(s)", lli);
-	printf("Size of a float: %d byte(s)", f);
+	printf("Size of a char: %d byte(s)\n", c);
+	printf("Size of an int: %d byte(s)\n", i);
+	printf("Size of a long int: %d byte(s)\n", li);
+	printf("Size of a long long int: %d byte(s)\n", lli);
+	printf("Size of a float: %d byte(s)\n", f);
 
 	return (0);
 }
