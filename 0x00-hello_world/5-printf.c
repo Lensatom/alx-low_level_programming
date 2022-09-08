6be7e2abc1f8556466381dcc03512fa2c8546e86
Lensatom <ayomidetommiwa@gmail.com>
2022-09-08 04:43:02 -0700

    Using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,12 +5,12 @@
  *
  * Description: Using printf to print texts
  *
- * return always 0 (success)
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return(0);
+	return (0);
 }
