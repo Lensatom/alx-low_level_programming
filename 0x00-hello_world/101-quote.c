4997957f43081a62610285b9be84110dc6134f3a
Lensatom <ayomidetommiwa@gmail.com>
2022-09-08 05:24:00 -0700

    Advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Printing to standard error
+ *
+ * Return: always 1 (success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
