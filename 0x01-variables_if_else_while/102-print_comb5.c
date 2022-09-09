27e008bc12417e1b138bdf6d88064449169a8cfa
Lensatom <ayomidetommiwa@gmail.com>
2022-09-09 10:18:41 -0700

    C Advaced Programming

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A program that prints combination of teo digit numbers
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = '0'; b < '9'; b++)
+		{
+			for (c = a + 1; c < '9'; c++)
+			{
+				for (d = b + 1; d < '9'; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (b < '8')
+					{
+						putchar(',');
+						putchar(' ');
+					};
+				};
+			};
+		};
+	};
+	putchar('\n');
+
+	return (0);
+}
