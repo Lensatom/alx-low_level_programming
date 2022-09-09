4c10e9401a18775ed0032b9ada65836ad134985c
Lensatom <ayomidetommiwa@gmail.com>
2022-09-09 11:53:07 -0700

    C Advanced Programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,14 +21,18 @@ int main(void)
 		{
 			for (c = '0'; c <= '9'; c++)
 			{
-				for (d = b + 1; d <= '9'; d++)
+				for (d = 0; d <= '9'; d++)
 				{
+					if (a == c || b == d)
+					{
+						continue;
+					};
 					putchar(a);
 					putchar(b);
 					putchar(' ');
 					putchar(c);
 					putchar(d);
-					if (a < '9' || b < '8')
+					if (a < '9' || b < '8' || c < '9' || d < '9')
 					{
 						putchar(',');
 						putchar(' ');
