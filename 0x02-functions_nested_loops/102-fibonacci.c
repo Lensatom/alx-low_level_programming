57119b186dcf2ede077c61271a0d137fb2e37437
Lensatom <ayomidetommiwa@gmail.com>
2022-09-14 09:22:05 -0700

    Alx task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,12 @@
 #include "stdio.h"
 
-
+/**
+ * main - entry point
+ *
+ * Description: A program that prints all number till 1024 that are divisible by 5 or 3
+ *
+ * Return: always 0 (success)
+ */
 
 int main(void)
 {
