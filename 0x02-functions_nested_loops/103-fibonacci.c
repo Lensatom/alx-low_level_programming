8ba71713f4a6ae00435de67e76fac922b9ecab66
Lensatom <ayomidetommiwa@gmail.com>
2022-09-14 09:31:30 -0700

    Alx task

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,30 +3,32 @@
 /**
  * main - entry point
  *
- * Description: A program that prints all number till 1024 that are divisible by 5 or 3
+ * Description: A program that prints the sum of even number till 4000000 in the fibonacci sequence
  *
  * Return: always 0 (success)
  */
 
 int main(void)
 {
-	int i;
-	unsigned long sum,
-	    fib1 = 0,
-	    fib2 = 1;
+	unsigned long result = 0,
+		      sum,
+		      fib1 = 0,
+		      fib2 = 1;
 
-	for (i = 0; i < 50; i++)
+	while (1)
 	{
 		sum = fib1 + fib2;
-		printf("%lu", sum);
-		if (i < 49)
+		if (sum <= 4000000 && sum % 2 == 0)
 		{
-			printf(", ");
+			result += sum;
+		} else if (sum > 4000000)
+		{
+			break;
 		};
 		fib1 = fib2;
 		fib2 = sum;
 	};
-	printf("\n");
+	printf("%lu\n", result);
 
 	return (0);
 }
