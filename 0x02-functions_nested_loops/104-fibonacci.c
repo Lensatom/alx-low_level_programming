9cb296425a18b7603426f4f3ad452b7dd3eb6d7b
Lensatom <ayomidetommiwa@gmail.com>
2022-09-14 09:40:06 -0700

    Alx task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,32 @@
-#include "main.h"
+#include "stdio.h"
 
-/*
+/**
  * main - entry point
  *
- * Description: A program that prints all numbers below 1024
+ * Description: A program that prints all number till 1024 that are divisible by 5 or 3
  *
  * Return: always 0 (success)
  */
 
 int main(void)
 {
-	int i,
-	    sum = 0;
+	int i;
+	unsigned long sum,
+	    fib1 = 0,
+	    fib2 = 1;
 
-	for (i = 0; i < 1024; i++)
+	for (i = 0; i < 50; i++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-		sum += i;
-	}
-	printf("%d\n", sum);
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+		if (i < 49)
+		{
+			printf(", ");
+		};
+		fib1 = fib2;
+		fib2 = sum;
+	};
+	printf("\n");
 
 	return (0);
 }
