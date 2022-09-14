56a7f10b2df911a4529e385f65dfa678e554e6db
Lensatom <ayomidetommiwa@gmail.com>
2022-09-14 06:43:47 -0700

    Alx task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,8 +26,9 @@ void print_times_table(int n)
 		};
 		putchar('0');
 		result = i;
-		for (j = 0; j <= n; j++)
+		for (j = 1; j <= n; j++)
 		{
+			putchar(',');
 			show(result);
 			if (j < n)
 			{
