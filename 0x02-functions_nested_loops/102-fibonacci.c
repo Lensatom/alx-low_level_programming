8ed42a57ea9eb5c23f4f9a5cc2b8758827ea2e6c
Lensatom <ayomidetommiwa@gmail.com>
2022-09-14 09:11:04 -0700

    Alx task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,18 +10,15 @@
 
 int main(void)
 {
-	int i,
-	    sum,
-	    fib1 = 1,
-	    fib2 = 2;
+	int i;
+	unsigned long sum,
+	    fib1 = 0,
+	    fib2 = 1;
 
-	printf("%d, %d", fib1, fib2);
 	for (i = 0; i < 50; i++)
 	{
-		fib1 = fib2;
-		fib2 = sum;
 		sum = fib1 + fib2;
-		printf("%d", sum);
+		printf("%lu", sum);
 		if (i < 49)
 		{
 			printf(" ,");
