c2be884452b365a8ed929a068c32f56fbf25e414
Lensatom <ayomidetommiwa@gmail.com>
2022-09-15 19:39:44 -0700

    Alx task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,11 +14,11 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < i; j++)
-		{
-			putchar(' ');
-		};
 		putchar('\\');
+		if (i == (n - 1))
+		{
+			continue;
+		}
 		putchar('\n');
 	};
 	putchar('\n');
