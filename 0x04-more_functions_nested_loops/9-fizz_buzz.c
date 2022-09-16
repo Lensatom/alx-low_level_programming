37fd948301a8f958b06bbdcf78b807aedb2c8c78
Lensatom <ayomidetommiwa@gmail.com>
2022-09-16 01:38:16 -0700

    Alx task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+
+/**
+* mian - A function that runs the fizz buzz algorithm
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		} else if (i % 5)
+		{
+			printf("Buzz ");
+		} else if (i % 3)
+		{
+			printf("Fizz ");
+		} else
+		{
+			print("%d ", i);
+		}
+	};
+
+	return (0);
+}
