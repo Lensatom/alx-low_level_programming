9c508f1fecf98129a4d9ffc508e2c9afd2a75987
Lensatom <ayomidetommiwa@gmail.com>
2022-09-16 01:49:52 -0700

    Alx task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if ((i % 15) == 0)
 		{
 			printf("FizzBuzz");
-		} else if (i % 5 == 0)
+		} else if ((i % 5) == 0)
 		{
 			printf("Buzz");
-		} else if (i % 3 == 0)
+		} else if ((i % 3) == 0)
 		{
 			printf("Fizz");
 		} else
@@ -29,6 +29,7 @@ int main(void)
 			printf(" ");
 		};
 	};
+	printf("\n");
 
 	return (0);
 }
