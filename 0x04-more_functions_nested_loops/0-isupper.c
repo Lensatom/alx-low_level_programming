72a6e22d1109d6d469adea6dbff636abb3972d75
Lensatom <ayomidetommiwa@gmail.com>
2022-09-15 18:27:08 -0700

    Alx task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,19 +2,15 @@
 
 /**
  * _isupper - returns a value for if the letter is uppercase or not
- *
  * @c: the undecided letter type
- *
  * Return: 0 or 1 (success)
  */
 
 int _isupper(int c)
 {
-	int decider,
-	    output;
+	int output;
 
-	decider = isupper(c);
-	if (decider > 0)
+	if (isupper(c))
 	{
 		output = 1;
 	} else
