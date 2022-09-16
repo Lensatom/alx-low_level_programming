3bc26a91da65e524f89880d24be6aa906b297b06
Lensatom <ayomidetommiwa@gmail.com>
2022-09-16 01:59:09 -0700

    Alx task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * print_square - prints out a square with size of the size parameter
- * @size: The magnitude in length of eac side
+ * print_triangle - prints out a triangle with size of the size parameter
+ * @size: The magnitude in length of supposed hypothenus and adjacent
  * Return: always void (success)
  */
 
-void print_square(int size)
+void print_triangle(int size)
 {
 	int i,
 	    j;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = size; j < 0; j--)
+		{
+			_putchar(' ');
+		};
+		for (k = 0; k < size; k++)
 		{
 			_putchar('#');
 		};
