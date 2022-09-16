eb187da9dfe92a9b0b7101ae44ae48896c88fcad
Lensatom <ayomidetommiwa@gmail.com>
2022-09-15 19:29:02 -0700

    Alx task

---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: number of times straight line should be printed
+ * Return: void (success)
+ */
+
+void print_diagonal(int n)
+{
+	int i,
+	    j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			putchar(' ');
+		};
+		putchar('\\');
+		putchar('\n');
+	};
+	putchar('\n');
+}
