aa13010b6e947370da502a77b6464997e27f1b24
Lensatom <ayomidetommiwa@gmail.com>
2022-09-16 01:04:46 -0700

    Alx task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,18 +11,12 @@ void print_square(int size)
 	int i,
 	    j;
 
-	for (i =0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if ((i == 0 || i == (size - 1)) || (j == 0 || j == (size - 1)))
-			{
-				putchar('#');
-			} else
-			{
-				putchar('0');
-			};
-			putchar('\n');
+			putchar('#');
 		};
+		putchar('\n');
 	};
 }
