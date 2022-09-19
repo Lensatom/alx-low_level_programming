ded166c40a8b06e94533016b62ba2798225b2ff5
Lensatom <ayomidetommiwa@gmail.com>
2022-09-19 04:42:28 -0700

    Alx task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strcpy - A function that copies an array and prints in to another address
+ * _strcpy - A function that copies an array and prints in to another address
  * @dest: The destination array
  * @src: The source array
  * Return: Always the modified destination array (success)
