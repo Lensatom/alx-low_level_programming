d0628e79bad4198f1521e08c1b1e5d836c559096
Lensatom <ayomidetommiwa@gmail.com>
2022-09-19 02:42:40 -0700

    Alx task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,11 +9,13 @@
 void rev_string(char *s)
 {
 	int len,
-	    i;
+	    i,
+	    reversed;
 
 	len = strlen(s);
 	for (i = (len - 1); i >= 0; i--)
 	{
-		*(s + i) = s[i];
+		reversed = reversed + s[i];
 	};
+	*s = reversed;
 }
