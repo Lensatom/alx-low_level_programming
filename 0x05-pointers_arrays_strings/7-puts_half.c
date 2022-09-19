803f13b3e36805aca5ce99c3b96ce6d05d628aee
Lensatom <ayomidetommiwa@gmail.com>
2022-09-19 03:44:41 -0700

    Alx task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * puts2 - A function that prints a every other character of a string
+ * puts_half - A function that prints the second half of a string
  * @str: The string to be printed
  * Return: Always void (success)
  */
 
-void puts2(char *str)
+void puts_half(char *str)
 {
 	int len,
 	    i;
 
 	len = strlen(str);
-	for (i = 0; i < len; i = i + 2)
+	i % 2 == 0 ? i = len / 2 : i = (len - 1) / 2;
+	for (i; i < len; i = i++)
 	{
 		_putchar(str[i]);
 	};
