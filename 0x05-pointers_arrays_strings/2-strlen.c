c258e374baa9cf9d911dd8c2e59f116c68db3fc9
Lensatom <ayomidetommiwa@gmail.com>
2022-09-19 00:55:23 -0700

    Alx task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,29 +5,14 @@ void print_count(int);
 /**
  * _strlen - A function that prints the length of a string
  * @str: The string to count
- * Return: Always void (success)
+ * Return: Always the length of str (success)
  */
 
-void _strlen(int *str)
+int _strlen(int *str)
 {
 	int len;
 
 	len = strlen(*str);
-	print_count(len);
-	_putchar((len % 10) + '0');
-	_putchar('\n');
-}
-
-/**
- * print_count - Prints any number with putchar
- * @num - The number to be printed
- */
-
-void print_count(int num)
-{
-	if (num / 10 > 0)
-	{
-		_putchar((num / 10) + '0');
-		print_count(num / 10);
-	};
+	
+	return (len);
 }
