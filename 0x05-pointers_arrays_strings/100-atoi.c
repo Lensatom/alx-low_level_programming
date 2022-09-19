1be415ddffbfa10902894556388d7d5ee9ea658c
Lensatom <ayomidetommiwa@gmail.com>
2022-09-19 07:00:13 -0700

    Alx task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int get_number(char, int, int);
+
 /**
  * _strcpy - A function that copies an array and prints in to another address
  * @dest: The destination array
@@ -10,32 +12,51 @@
 int _atoi(char *s)
 {
 	int i = 0,
-	    j,
 	    start,
 	    end,
 	    negative,
-	    positive,
 	    len = strlen(s);
 
 	while (i < len)
 	{
-		if (s[i] == '+')
-			positive++;
-		else if (s[i] == '-')
+		if (s[i] == '-')
 			negative++;
 		else if (isdigit(s[i]))
 		{
 			start = i;
-			j = i;
+			end = i;
 			while (1)
 			{
-				if (!(isdigit(s[j])))
+				if (!(isdigit(s[end])))
 					break;
-				j++;
+				end++;
 			};
-			end = j - 1;
 		};
 	};
+	if (negative % 2 == 1)
+		num = get_number(s, start, end) * -1;
+	num = get_number(s, start, end);
 
-	return (start, end);
+	return (num);
 }
+
+/**
+ * get_number - The function that gets the actual number from the string
+ * @s: the string
+ * @start: the number start point
+ * @end: the number end point (exclusive)
+ * Return: the real number
+ */
+
+int get_number(char s, int start, int end)
+{
+	int hrcy,
+	    num;
+
+	hrcy = end - start;
+	for (i = 0; i < hrcy; i++)
+	{
+		num = num + atoi(s[start + i]) + (10 ** i)
+	}
+
+	return num;
