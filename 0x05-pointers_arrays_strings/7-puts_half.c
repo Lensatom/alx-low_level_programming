bd710c96e37d73a67d4d922fb041133182f12f2e
Lensatom <ayomidetommiwa@gmail.com>
2022-09-19 03:52:14 -0700

    Alx task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,8 +12,13 @@ void puts_half(char *str)
 	    i;
 
 	len = strlen(str);
-	
-	for (i = len / 2; i < len; i++)
+
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len - 1) / 2;
+
+	while (i < len)
 	{
 		_putchar(str[i]);
 	};
