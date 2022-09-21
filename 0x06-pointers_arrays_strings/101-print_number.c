ac70a09526db28cd86134dfb8022616b646a3ae4
Lensatom <ayomidetommiwa@gmail.com>
2022-09-21 13:53:02 -0700

    Alx task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,14 +9,16 @@
 void print_number(int n)
 {
 	unsigned int num = n;
-	int div;
 
 	div = num / 10;
 	if (n < 0)
-		putchar('-');
-	if (div > 0)
 	{
-		print_number(div);
-	}
-	putchar((num % 10) + '0');
+		_putchar('-');
+		num = -num;
+	};
+	if ((num / 10) > 0)
+	{
+		print_number(num / 10);
+	};
+	_putchar((num % 10) + '0');
 }
