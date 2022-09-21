5fb35f3bbe004ea6de1c7ac40a382d42742dd545
Lensatom <ayomidetommiwa@gmail.com>
2022-09-21 08:54:48 -0700

    Alx task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,6 +4,7 @@
  * _strncat - A function that concatenates a string to n chars from another
  * @dest: The host string that will hold the result
  * @src: The added string
+ * @n: The number of characters of n to be added
  * Return: Always the concatenated string
  */
 
