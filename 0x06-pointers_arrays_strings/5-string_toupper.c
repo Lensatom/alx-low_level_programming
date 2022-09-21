14953663e34d55e964db1212be9249bb0e83526f
Lensatom <ayomidetommiwa@gmail.com>
2022-09-21 08:25:32 -0700

    Alx task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * string_toupper - A function that converts a string to all uppercase
+ * @str - The string to be converted
+ * Return: Always the converted string
+ */
+
+char *string_toupper(char *str)
+{
+	return (toupper(str));
+}
