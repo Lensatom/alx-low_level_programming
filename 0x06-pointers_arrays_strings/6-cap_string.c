f952b5dd956b170852c7104eb7caaa94cd79400b
Lensatom <ayomidetommiwa@gmail.com>
2022-09-21 12:48:21 -0700

    Alx task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,12 +14,12 @@ char *cap_string(char *str)
 	    j;
 
 	len = strlen(str);
+	str[0] = toupper(str[0])
 	for (i = 0; i < len; i++)
 	{
 		for (j = 0; j < 13; j++)
 		{
-			if ((str[i] == sprts[j]) &&
-(toupper(str[i + 1]) >= 'A' && toupper(str[i + 1]) <= 'Z'))
+			if (str[i] == sprts[j])
 				str[i + 1] = toupper(str[i + 1]);
 		}
 	}
