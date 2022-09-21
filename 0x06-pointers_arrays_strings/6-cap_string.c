cf68483a642936c7900ccfb75c9fb11e91f3ddeb
Lensatom <ayomidetommiwa@gmail.com>
2022-09-21 08:37:35 -0700

    Alx task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes each word in a string
+ * @str: The input string
+ * return: Always the capitalixed string
+ */
+
+char *cap_string(char *str)
+{
+	char sprts[13] = " \t\n,;.!?\"(){}";
+	int len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == sprts[j])
+				str[i + 1] = toupper(str[i + 1]);
+		}
+	}
+
+	return (str);
+}
