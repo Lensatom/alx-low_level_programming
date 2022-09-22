d3629a81b4a34c9e7237e6cdcd2514ec962ba44f
Lensatom <ayomidetommiwa@gmail.com>
2022-09-22 05:56:50 -0700

    Alx task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	printf("%p\n%p\n", p, &a);
+	printf("%p\n", p - &a);
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
