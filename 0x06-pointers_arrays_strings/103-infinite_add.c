ad0ed260a326085707f9452df4e49dc14c715c69
Lensatom <ayomidetommiwa@gmail.com>
2022-09-21 17:23:07 -0700

    Alx task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,13 +10,25 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	unsigned int add;
+	int num,
+	    tens = 0;
 
-	add = n1 + n2;
-	if (sizeof(add) <= size_r)
-		r = add;
-	else
-		r = 0;
-
-	return (r)
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+	};
+	for (; *n1, n1--; r_index++)
+	{
+		num = *(n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	};
+	for (; n2, n2--; r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	};
 }
