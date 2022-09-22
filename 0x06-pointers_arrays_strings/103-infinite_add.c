67287fdc60efd15d2daf654dded2da26703e0575
Lensatom <ayomidetommiwa@gmail.com>
2022-09-22 06:41:10 -0700

    Alx task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,10 +2,11 @@
 
 /**
  * infinite_add - A function that adds two numbers
- * n1: the first number to add
- * n2: the second number to add
- * r: the buffer to store the result in
- * size_r: the size of the buffer
+ * @n1: the first number to add
+ * @n2: the second number to add
+ * @r: the buffer to store the result in
+ * @size_r: the size of the buffer
+ * Return: Always the added number of 0 if size_r is small
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
