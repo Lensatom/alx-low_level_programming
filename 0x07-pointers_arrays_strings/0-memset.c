274e5efac2ea5ca9d44941e7d4e0c29fa9e21c02
Lensatom <ayomidetommiwa@gmail.com>
2022-09-26 15:27:55 -0700

    copying vytes from memiry to memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,23 @@
 #include "main.h"
-
-/**
- * _memset - A function that fills the first n bytes (excluded)
- * of the memory area pointed to by s with the constant byte c
- * @s: The pointer to the variable to be populated
- * @b: The variable containing the bytes to be transferred
- * @n: The number of bytes to be populated in s
- * Return: Always the pointer to the memory area (success)
- */
-
-char *_memset(char *s, char b, unsigned int n);
+ 
+ /**
+  * * _memset - Fills the first n bytes of the memory area
+  * * pointed to by @s with the constant byte @c.
+  * * @s: A pointer to the memory area to be filled.
+  * * @c: The character to fill the memory area with.
+  * * @n: The number of bytes to be filled.
+  * * description _memset: over there
+  * *
+  * *Return: A pointer to the filled memory area @s
+  * *
+  * */
+void *_memset(void *s, int c, size_t n)
 {
-	memset(s, b, n);
+unsigned int index;
+unsigned char *memory = s, value = c;
 
-	return (s);
+for (index = 0; index < n; index++)
+	memory[index] = value;
+
+return (memory);
 }
