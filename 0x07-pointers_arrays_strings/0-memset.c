c07a37e4523795c29d3ef55644273bb99c417df7
Lensatom <ayomidetommiwa@gmail.com>
2022-09-26 08:11:26 -0700

    Alx task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - A function that fills the first n bytes (excluded)
+ * of the memory area pointed to by s with the constant byte b
+ * @s: The pointer to the variable to be populated
+ * @b: The variable containing the bytes to be transferred
+ * @n: The number of bytes to be populated in s
+ * Return: Always the pointer to the memory area, s (success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+
+	return (s)
+}
