19d2cd40400e2a30508e81a93fa731d8f63011d9
Lensatom <ayomidetommiwa@gmail.com>
2022-09-26 22:39:50 -0700

    character finding algorithm in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,21 @@
 #include "main.h"
 
+/**
+ * _strchr - Finds a character in a string, s
+ * @s: The string to be searched
+ * @c: The character to be searched for
+ * Return: Thr character if found or 0 is not found
+ */
+
 char *_strchr(char *s, char c)
 {
-	char *result = strchr(s, c);
+	int i;
 
-	return (result);
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+
+		return (0);
+	}
 }
