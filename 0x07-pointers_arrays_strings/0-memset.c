05b6c67c505d67c2d6d7d7fd2b864b8474f7d163
Lensatom <ayomidetommiwa@gmail.com>
2022-09-27 02:44:13 -0700

    Pushed initial code. Looks like the checker is broken :(

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,4 +8,13 @@
  * Return: A pointer to the filled memory area
  */
 
-hello
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int index;
+	unsigned char *memory = s, value = c;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
+}
