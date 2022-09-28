28e5835c62a36ea1ba24ea98986969a3cb10e070
Lensatom <ayomidetommiwa@gmail.com>
2022-09-28 03:57:48 -0700

    algorithm to find sqare root recursively

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - A fjnction that finds the square root of n
+ * @n: The number whose square root has to be found
+ * Return: the square root of n or -1 if n is not a perfect square
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (iterator(n, 0));
+}
+
+/**
+ * iterator - Funtion that iterates possible sqare roots recursively
+ * @n: The number's square root to be found
+ * @a: Possible sqyare roots
+ * Return: The square root of n or -1 if n is not a perfect square
+ */
+
+int iterator(n, a)
+{
+	if (a * a == n)
+		return (a);
+	else if (a == n / 2)
+		return (-1);
+	else
+		return (iterator(n, a + 1));
+}
