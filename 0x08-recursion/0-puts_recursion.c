68fb21302aa444f45837a4b55be46e8d35eb95f2
Lensatom <ayomidetommiwa@gmail.com>
2022-09-28 01:42:08 -0700

    Printing strings

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - A function that prints a string
+ * @s: The string to ve printed
+ * Return: Always void (success)
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+#include "stdio.h"
+#include "string.h"
+#include "stddef.h"
+#include "ctype.h"
+
+void _puts_recursion(char *s);
