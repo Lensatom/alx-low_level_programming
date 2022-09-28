1a3e3f3b4031adc935e4190e64e3a447fbf08ca8
Lensatom <ayomidetommiwa@gmail.com>
2022-09-28 02:47:00 -0700

    Debigging reverse printing

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,18 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	unsigned int len,
-		     i;
-	char new[999];
-
-	len = strlen(s);
-	if (len > 1)
+	if (*s != '\0')
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		for(i = 0; i < len - 1; i++)
-			new[i] = *(s + i);
-		_print_rev_recursion(new);
-	};
-	_putchar(*s);
-	_putchar('\n');
+	}
 }
