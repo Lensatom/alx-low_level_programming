5cc0ca7af7643d23e9a845d5f54d6a3b45540d02
Lensatom <ayomidetommiwa@gmail.com>
2022-09-28 04:17:08 -0700

    Checking if a number is prime or not recyrsuvely

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int iterator(int n, int a);
+
+/**
+ * is_prime_number - A function that checks if n is a prime number or otherwise
+ * @n: The number to check
+ * Return: 1 if prime and 0 if otherwise
+ */
+
+int is_prime_number(int n)
+{
+	iterator(n, 2);
+}
+
+/**
+ * iterator - A function that iterates numbers to test n
+ * @n: The number to check
+ * @a: The numbers trying to divide n
+ * Return: 1 if prime and 0 if otherwise
+ */
+
+int iterator(int n, int a)
+{
+	if (n <= 0)
+		return (0);
+	else if (n == a)
+		return (1);
+	else
+		return (iterator(n, a + 1));
+}
