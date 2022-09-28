4dfb88fd6a9cbfaa1565b801bfaa7d566bac420d
Lensatom <ayomidetommiwa@gmail.com>
2022-09-28 07:41:18 -0700

    An algorithm to check if a string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * is_palindrome - Checks if a stribg is the same backwards
+ * @s: The string to check
+ * Return: 1 if true and 0 if otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	return (iterator(s, 0, strlen(s)));
+}
+
+/**
+ * iterator: A funtion that iterates over characters in s
+ * @s: The input string
+ * @a: The lower limit of char to check
+ * @b: The upper limit of char to check
+ * Return: 1 if a and b are tge same allthrough and 0 if otherwise
+ */
+
+int iterator(char *s, a, b)
+{
+	/* An empty string is a palindrome */
+	if (a == b)
+		return (1);
+	else if (s[a] != s[b])
+		return (0);
+	else
+		return (iterator(s, a + 1, b - 1));
+}
