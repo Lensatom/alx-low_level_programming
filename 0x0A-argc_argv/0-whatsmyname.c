bdc5bfe668d270fff5636e902553e251fc3f0eb7
Lensatom <ayomidetommiwa@gmail.com>
2022-09-30 05:18:33 -0700

    A program that kniws it own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+/**
+ * main - A program that prints this filename
+ * @argc: The length of commands in the command line
+ * @argv: The items in the command ijnthe command line
+ * Return: Always void (success)
+ */
+
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
