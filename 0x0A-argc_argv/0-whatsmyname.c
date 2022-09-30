164043120908e33242e8b96606bed963a796b80d
Lensatom <ayomidetommiwa@gmail.com>
2022-09-30 05:29:35 -0700

    A program that knows it own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "string.h"
 
 /**
  * main - A program that prints this filename
@@ -9,5 +10,10 @@
 
 void main(int argc, char *argv[])
 {
+	int i;
+	char filename[999] = argv[0];
+
+	for (i = 0; i < strlen(filename); i++)
+		_putchar(filename[i];
 	printf("%s\n", argv[0]);
 }
