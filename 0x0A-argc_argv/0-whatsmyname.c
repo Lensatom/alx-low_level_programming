69e5337f35102c65901aca0754cec062e3b6e8ac
Lensatom <ayomidetommiwa@gmail.com>
2022-09-30 06:18:49 -0700

    A program that knows it name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include "stdio.h"
 
 /**
- * my_name - A program that prints this filename
+ * main - A program that prints this filename
  * @argc: The length of commands in the command line
  * @argv: The items in the command ijnthe command line
- * Return: Always void (success)
+ * Return: Always 0 (success)
  */
 
-int my_name(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
