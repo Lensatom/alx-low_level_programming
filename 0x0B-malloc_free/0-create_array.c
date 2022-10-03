d2bf06efc88206d056bb82f51b03ebda8773cc14
Lensatom <ayomidetommiwa@gmail.com>
2022-10-03 12:10:49 -0700

    Debugging

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	*arr = (char *)malloc(size);
+	arr = (char *)malloc(size);
 	for (i = 0; i < size; i++)
 		*(arr + i) = c;
 
