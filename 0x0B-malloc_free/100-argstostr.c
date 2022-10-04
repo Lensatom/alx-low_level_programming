7d3711da5c5fc25a13e05985266f95c32626e7c8
Ayomide Atayero <77001715+Lensatom@users.noreply.github.com>
2022-10-04 23:39:29 +0100

    Uodate

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,47 +1,40 @@
 #include "main.h"
 
 /**
- * argstostr -  A function that cancatenates all it arguments
- * @ac: The length of the arguments (argc)
- * @av: The arguments (argv)
- * Return: A pointer to the concatenated (success) NULL (fail)
+ * argstostr - concatenates all arguments into a new string
+ * @ac: the argument count
+ * @av: the argument vector
+ *
+ * Return: NULL memory allocation fails, ac is 0 or av is NULL
+ * otherwise return a pointer to the new string
  */
-
 char *argstostr(int ac, char **av)
 {
-	char *cat;
-	int i,
-	    j,
-	    k,
-	    m,
-	    leng,
-	    space;
+	char *cat, *chr;
+	int arg, size;
 
-	if (ac <= 0 || av == NULL)
+	if (!ac || !av)
 		return (NULL);
-	/** Allocating enough space **/
-	space = 0;
-	for (m = 0; m < ac; m++)
-		space = space + strlen(av[m]);
-	/** Making memory available for '\n' **/
-	space = space + ac;
-	cat = (char *)malloc(sizeof(char) * space);
-	if (cat == NULL)
-		return (NULL);
-	k = -1;
-	while (i < space)
+
+	for (arg = 0, size = 1; arg < ac; ++arg, ++size)
 	{
-		k++;
-		leng = strlen(av[k]);
-		for (j = 0; j <= leng; j++)
-		{
-			if (j == leng)
-				cat[i] = '\n';
-			else
-				cat[i] = av[k][j];
-			i++;
-		}
-	};
+		for (chr = av[arg]; *chr; ++chr, ++size)
+			;
+	}
+
+	cat = (char *) malloc(sizeof(char) * size);
+
+	if (!cat)
+		return (NULL);
+
+	for (arg = 0, size = 0; arg < ac; ++arg, ++size)
+	{
+		for (chr = av[arg]; *chr; ++chr, ++size)
+			cat[size] = *chr;
+		cat[size] = '\n';
+	}
+
+	cat[size] = '\0';
 
 	return (cat);
 }
