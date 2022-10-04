48a9511e43da095da290194e525643844ff1ad95
Ayomide Atayero <77001715+Lensatom@users.noreply.github.com>
2022-10-04 15:19:45 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,11 +19,31 @@ int **alloc_grid(int width, int height)
 	arr = (int *)malloc(sizeof(int) * width * height);
 	if (arr == NULL)
 		return (NULL);
+	p = (int **) malloc(height = sizeof(int *));
+	/* we have to make a malloc per pointer */
+	if (!p)
+	{
+		free(p);
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < width; j++)
-			arr[i][j] = 0;
-	};
-
-	return (arr);
+		p[i] = (int *) malloc(width * sizeof(int));
+		if (!p[i])
+		{
+			for (j = 0; j <= i; j++)
+				free(p[j]);
+			free(p);
+			return (NULL);
+		}
+	}
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < width; b++)
+		{
+			p[a][b] = 0;
+		}
+	}
+	
+	return (p);
 }
