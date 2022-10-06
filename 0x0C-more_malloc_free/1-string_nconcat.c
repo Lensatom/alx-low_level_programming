685ca2420d4818095fd9ec894167a5515609d1b5
Lensatom <ayomidetommiwa@gmail.com>
2022-10-06 06:36:09 -0700

    A function thatbworks like strncpy with malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Contatenates two string with n bytes from the guest stribg
+ * @s1: Yhe host string
+ * @s2: The guest string
+ * @n: The number of bytes to ve removed from s2
+ * Return: A pointer to the newly allocated memory (success)
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *home;
+	unsigned int i,
+		     leng;
+
+	leng = strlen(s1) + n;
+	home = (cahr *)malloc(sizeof(char) * leng + 1);
+	/* Check if malloc worked */
+	if (home == NULL)
+		return NULL;
+	for (i = 0; i < strlen(s1); i++)
+		*(home + i) = s1[i];
+	for (i = 0; i < n; i++)
+		*(home + strlen(s1) + i) = s2[i];
+
+	return (home);
+}
