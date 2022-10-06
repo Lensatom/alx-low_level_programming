3842b411c4947a1ade0c1512f87fdb784a4c8703
Lensatom <ayomidetommiwa@gmail.com>
2022-10-06 08:23:51 -0700

    A functiin that returns a range of numbers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * array_range - A function that allocates memory to an array (excluded)
+ * with elements having a size
+ * @nmemb: Size of each element
+ * @size: size of the array
+ * Return: A pointer to the created (success) NULL (fail)
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	unsigned int i;
+
+	if (min > max)
+		return (NULL);
+	ptr = (int *)malloc(sizeof(int) * max + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; main <= max; i++)
+	{
+		*(ptr + i) = min;
+		min++;
+	}
+
+	return (ptr);
+}
