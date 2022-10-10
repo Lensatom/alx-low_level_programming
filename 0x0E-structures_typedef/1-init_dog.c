ef56b05dd83f1cfc9c84c8c6fa6a2cf743f72d31
Ayomide Atayero <77001715+Lensatom@users.noreply.github.com>
2022-10-10 15:59:09 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,6 +7,7 @@
  * @name: A member of the struct
  * @age: A member of the struct
  * @owner: A member of the struct
+ * Return: Always void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
