6e4c161a6fc57b79a768938ae08e0fca43e556ba
Lensatom <ayomidetommiwa@gmail.com>
2022-10-10 06:07:22 -0700

    Solved double inclusion problem

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef ABS
+#ifndef _FILE_NAME_H_
+#define _FILE_NAME_H_
+
 #define ABS(x) ((x) < (0) ? (x * -1) : (x))
+
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,6 @@
-#ifndef SUM
+#ifndef _FILE_NAME_H_
+#define _FILE_NAME_H_
+
 #define SUM(x, y) ((x) + (y))
+
 #endif
