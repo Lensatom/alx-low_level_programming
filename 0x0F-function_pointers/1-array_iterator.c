6d1e5d90df569f7cda3a6ff26c45e0348884590d
Lensatom <ayomidetommiwa@gmail.com>
2022-10-12 11:57:28 -0700

    Debugging

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (array != NULL && action != NULL)
+	if (array != NULL && action != NULL && size > 0)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
