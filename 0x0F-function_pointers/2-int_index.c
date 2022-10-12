e8b70772d025acdf21116b1b4453f41831004757
Lensatom <ayomidetommiwa@gmail.com>
2022-10-12 12:01:50 -0700

    Implementing safe coding

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,9 +9,10 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i,
+	    isFound;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
