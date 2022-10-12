9a9018cf17c7961d74b473e03d87a513154ee461
Ayomide Atayero <77001715+Lensatom@users.noreply.github.com>
2022-10-12 20:22:46 +0100

    Added the main file

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include "function_pointers.h"
+
+/**
+ * main - program that prints the opcode of its own main function
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ *
+ * Return: On success, 1 or 2 in case of failure
+ */
+int main(int argc, char *argv[])
+{
+	int index, bytes;
+	int (*address)(int, char**) = main;
+	unsigned char upcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < bytes; index++)
+	{
+		upcode = *(unsigned char *)address;
+		printf("%.2x", upcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+		address++;
+	}
+	printf("\n");
+	return (0);
+}
