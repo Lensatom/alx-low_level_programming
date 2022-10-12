5d6ed24d515961e4d12a82529706dce69a2c4ed7
Lensatom <ayomidetommiwa@gmail.com>
2022-10-12 11:41:53 -0700

    Added safe code and a header file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include "function_pointers.h"
 
 /**
  * print_name - Print a name using a pointer to a function
@@ -8,5 +8,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
