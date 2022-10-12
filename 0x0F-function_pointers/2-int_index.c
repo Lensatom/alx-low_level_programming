631ada918c07d3914e9e57eba1786780772be18d
Lensatom <ayomidetommiwa@gmail.com>
2022-10-12 12:05:40 -0700

    Corrected betty documentation requirements

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,7 +4,8 @@
  * int_index - A function that calls a function to search in an array
  * @array: The array to be iterated
  * @size: The size of the array
- * return: The the index to the found (success) -1 (fail)
+ * @cmp: The function that compares
+ * Return: The the index to the found (success) -1 (fail)
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
