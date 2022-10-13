91d5784f13b1cf449b0e80e207ebe3af8d2bedbe
Lensatom <ayomidetommiwa@gmail.com>
2022-10-13 09:08:01 -0700

    Correct a typo in the 1th file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ap, int));
 		/* Check if there is a seperator */
-		if (seperator != NULL && i == n - 1)
+		if (separator != NULL && i == n - 1)
 			printf("%s", seperator);
 	}
 	printf("\n");
