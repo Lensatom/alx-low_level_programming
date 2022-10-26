05f87ca85cf70adb95b24c81d2c9e3d48aaf89ad
Lensatom <ayomidetommiwa@gmail.com>
2022-10-26 12:21:24 -0700

    Hello

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,23 +20,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int node;
 
 	if (copy == NULL)
-
 		return (-1);
-
 	if (index == 0)
-
 	{
-
 		*head = (*head)->next;
-
 		free(copy);
-
 		return (1);
-
 	}
-
 	for (node = 0; node < (index - 1); node++)
-
 	{
 
 		if (copy->next == NULL)
